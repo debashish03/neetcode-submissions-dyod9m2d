b65b909c75aac0513e67e631f38acb5c7a63df20
DEBASHISH DASH <debashishdash03@gmail.com>
2026-04-11 06:53:12 +0530

    Add: merge-two-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-0.swift
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     var val: Int
+ *     var next: ListNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+        if list1 == nil {
+            return list2
+        }
+
+        if list2 == nil {
+            return list1
+        }
+
+        if list1!.val <= list2!.val {
+            list1!.next = mergeTwoLists(list1!.next, list2)
+            return list1
+        } else {
+            list2!.next = mergeTwoLists(list1, list2!.next)
+            return list2
+        }
+    }
+}
